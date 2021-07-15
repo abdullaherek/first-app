0f00d91279100b6818311033d2439d4ca311d579
abdullaherek <abdullah.erek@ogrenci.bartin.edu.tr>
2021-07-15 04:16:17 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
+import alertify from "alertifyjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { currentCategory: "", products: [], cart: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+  };
+
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  addToCart = (product) => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find(c=>c.product.id === product.id);
+    if(addedItem){
+      addedItem.quantity +=1;
+    }
+    else{
+      newCart.push({ product: product, quantity: 1 });
+    }
+    
+    this.setState({ cart: newCart });
+    alertify.success(product.productName + " added to cart!");
+  }
+
+  removeFromCart = (product) =>{
+    let newCart = this.state.cart.filter(c=>c.product.id!==product.id);
+    this.setState({cart:newCart})
+  }
+
+  render() {
+    let categoryInfo = { title: "Category List" };
+    let productInfo = { title: "Product List" };
+    return (
+      <div className="App">
+        <Container>
+          <Navi removeFromCart={this.removeFromCart} cart={this.state.cart} />
+          <Row>
+            <Col xs="6">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="6">
+              <ProductList
+                products={this.state.products}
+                addToCart={this.addToCart}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/CategoryList.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+export default class Category extends Component {
+  /* constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
+  }*/
+
+  state = {
+    categories: []
+  };
+  componentDidMount(){
+    this.getCategories();
+  }
+  getCategories = () =>{
+    fetch("http://localhost:3000/categories")
+    .then(response => response.json())
+    .then(response => this.setState({categories:response}));;
+    
+  }
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}</h3>
+        <ListGroup>
+          {this.state.categories.map(category => (
+            <ListGroupItem active={category.categoryName===this.props.currentCategory?true:false}
+              onClick={() => this.props.changeCategory(category)}
+              key={category.id}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+        <h4>{this.props.currentCategory}</h4>
+      </div>
+    );
+  }
+}
